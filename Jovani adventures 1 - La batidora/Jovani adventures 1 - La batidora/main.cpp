486be5de32c8123434a02e60f198f25c8cb08f37
Ferran Llobet <ferran.llobet.xatart@estudiantat.upc.edu>
2023-03-12 18:01:00 +0100

    Upload

---
+++ Jovani adventures 1 - La batidora/Jovani adventures 1 - La batidora/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+#pragma region Player
+
+#pragma endregion
+
+
+#pragma region Audio
+
+#pragma endregion
+
+
+	return 0;
+}
