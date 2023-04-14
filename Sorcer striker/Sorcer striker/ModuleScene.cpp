cf15ce79c63aebc9cdb18e7403e62eb5d812cdf6
miguelturmo <125253347+miguelturmo@users.noreply.github.com>
2023-04-14 18:38:24 +0200

    ningun cambio

--- Sorcer striker/Sorcer striker/ModuleScene.cpp
+++ Sorcer striker/Sorcer striker/ModuleScene.cpp
@@ -23,7 +23,7 @@ bool ModuleScene::Start()
 
 	bool ret = true;
 
-	bgTexture = App->textures->Load("../Assets/Fondos-1.0.png");
+	bgTexture = App->textures->Load("../Assets/Fondo.png");
 
 	return ret;
 }
