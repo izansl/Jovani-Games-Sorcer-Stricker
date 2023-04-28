6f95bcefc8d19217514755b034f4f613f583c094
Lluccib <lluc.cid@estudiantat.upc.edu>
2023-04-28 20:57:21 +0200

    Adiciones Intro
    Modificaciones en:
    
    SceneIntro.cpp:
    -Modificando Start() y PostUpdate()
    SceneIntro.h:
    -Añadiendo texturas
    Filenames.h:
    -Añadiendo ImagesIntro en .bmp

--- Sorcer striker el bueno/Sorcer striker el bueno/Modules/Gameplay/SceneIntro.cpp
+++ Sorcer striker el bueno/Sorcer striker el bueno/Modules/Gameplay/SceneIntro.cpp
@@ -19,16 +19,20 @@ bool SceneIntro::Start() {
 	LOG("Loading background assets SCENE INTRO");
 
 	bool ret = true;
-
+	//Carga de texturas(imagenes)
 	bgTexture = App->textures->Load(FI_Introimage_1.c_str());
-	App->audio->PlayMusic(FA_Music_introTitle.c_str(), 1.0f);
+	SecondTexture = App->textures->Load(FI_Introimage_1.c_str());
+
+	//Carga de Audio ////TURMO MUY IMPORTANTE, TIENES QUE CUADRAR EL AUDIO CON LA INTRO SEGUN LAS IMAGENES QUE APAREZCAN///
+	App->audio->PlayMusic(FA_Music_introTitle.c_str(), 1.0f);//esta musica hay que cambiarla turmo
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
 
 	return ret;
-}
+} 
 
+//NO TOCAR NADA
 Update_Status SceneIntro::Update() {
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN) {
 		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
@@ -43,7 +47,14 @@ Update_Status SceneIntro::Update() {
 // Update: draw background
 Update_Status SceneIntro::PostUpdate() {
 	// Draw everything --------------------------------------
+	float timer = 0.0f;
+
 	App->render->Blit(bgTexture, 0, 0, NULL);
+	if (timer < 3.0f) {
+		App->render->Blit(SecondTexture, 0, 0, NULL);
+	}
+	//A CONTINUAR
+	timer += App->getdeltatime();
 
 	return Update_Status::UPDATE_CONTINUE;
 }
--- Sorcer striker el bueno/Sorcer striker el bueno/Modules/Gameplay/SceneIntro.h
+++ Sorcer striker el bueno/Sorcer striker el bueno/Modules/Gameplay/SceneIntro.h
@@ -31,6 +31,7 @@ public:
 public:
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* bgTexture = nullptr;
+	SDL_Texture* SecondTexture = nullptr;///////////////////////////
 	uint tokenFx = 0;
 };
 
