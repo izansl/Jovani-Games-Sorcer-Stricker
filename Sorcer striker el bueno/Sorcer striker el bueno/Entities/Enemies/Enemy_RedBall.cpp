214df6e32c831f788a934fbd484dc3e4e3d4d5dc
izansl <125253405+izansl@users.noreply.github.com>
2023-05-04 23:51:22 +0200

    Cambio bollared
    Bollared

--- Sorcer striker el bueno/Sorcer striker el bueno/Entities/Enemies/Enemy_RedBall.cpp
+++ Sorcer striker el bueno/Sorcer striker el bueno/Entities/Enemies/Enemy_RedBall.cpp
@@ -537,6 +537,7 @@ void Enemy_RedBall::OnCollision(Collider* c1) {
 	fly.PushBack({ 42, 50, 32, 32 });
 	fly.PushBack({ 77, 51, 32, 32 });
 	fly.PushBack({ 111, 52, 32, 32 });
+	fly.PushBack({ 200, 200, 32, 32 });
 
 	currentAnim = &fly;
 	fly.speed = 0.2;
