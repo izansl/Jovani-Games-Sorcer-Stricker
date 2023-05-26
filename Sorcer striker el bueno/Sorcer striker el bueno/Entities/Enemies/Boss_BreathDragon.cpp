404167ffb04c0c69da7b542e2b9d736dddca25cc
Ferran-LX <115022293+Ferran-LX@users.noreply.github.com>
2023-05-26 09:54:08 +0200

    Update boss

---
+++ Sorcer striker el bueno/Sorcer striker el bueno/Entities/Enemies/Boss_BreathDragon.cpp
@@ -0,0 +1,25 @@
+#include "Boss_BreathDragon.h"
+
+Boss_BreathDragon::Boss_BreathDragon(int x, int y)
+{
+}
+
+void Boss_BreathDragon::Update()
+{
+}
+
+void Boss_BreathDragon::OnCollisionGeneral(Collider* c1)
+{
+}
+
+void Boss_BreathDragon::OnCollisionHead1(Collider* c1)
+{
+}
+
+void Boss_BreathDragon::OnCollisionHead2(Collider* c1)
+{
+}
+
+void Boss_BreathDragon::OnCollisionHead3(Collider* c1)
+{
+}
