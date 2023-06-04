501a6343b4b510fc16ec42a3d88ac0bc5e12ecf7
izansl <125253405+izansl@users.noreply.github.com>
2023-06-05 00:44:29 +0200

    Stageclear
    -Añadido timer a stage clear

--- Sorcer striker/Sorcer striker/Entities/Enemies/Enemy_Stageclear.cpp
+++ Sorcer striker/Sorcer striker/Entities/Enemies/Enemy_Stageclear.cpp
@@ -8,6 +8,10 @@
 #include"../../Modules/Core/ModuleTextures.h"
 
 
+#include <SDL_timer.h>
+
+
+
 Enemy_Sclear::Enemy_Sclear(int x, int y) : Enemy(x, y) {
 	texture = App->textures->Load(FI_imageplayer_kill.c_str());
 
@@ -21,7 +25,7 @@ Enemy_Sclear::Enemy_Sclear(int x, int y) : Enemy(x, y) {
 
 	currentPath = &pathchest;
 
-
+	start_time = SDL_GetTicks();
 }
 
 void Enemy_Sclear::Update() {
@@ -29,6 +33,11 @@ void Enemy_Sclear::Update() {
 	pathchest.Update();
 	position = spawnPos + currentPath->GetRelativePosition();
 
+	if (SDL_GetTicks() - start_time >= 8000)
+	{
+		pickup.PushBack({ 10000, 1037, 688, 875 });
+		currentAnim = &pickup;
+	}
 	// Call to the base class. It must be called at the end
 	// It will update the collider depending on the position 
 	Enemy::Update();
